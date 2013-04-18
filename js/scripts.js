8a24e0ce605c8d95db9e466418ea0c97a868e11c
Gabriel Izaias <gabriel.izaias@gmail.com>
2013-04-18 13:37:45 -0300

    Abstrai script de validação
    Agora pode ser usado em vários formulários ao invés de ter que copiar e
    colar o código pra todos

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
+
 /**
- * Login
+ * Valida o formulário
+ *
+ * @param  {object}  form  O formulário à ser validado
+ * @return {boolean}
  */
-var login = document.getElementById('login');
-// Se existir o form de login, validar campos
-if (login) {
-    // "Escuta" o evento `onsubmit` do form
-    login.onsubmit = function(e) {
+function validar(e, form) {
+    if (form) {
         // Inicializa a variavel que mostra se tem erro ou não
         var erro = false;
         // Inicializa a mensagem de erro
         var mensagem = 'Favor preencher os campos abaixo: \n\n';
 
         // Loop em cada elemento do formulário
-        for (var i = 0; i < login.elements.length; i++) {
+        for (var i = 0; i < form.elements.length; i++) {
             // Guarda o elemento atual pra evitar a fadiga
-            var input = login.elements[i];
+            var input = form.elements[i];
             // Valida o input
             if (podeValidar(input) && input.value == '') {
                 // Adiciona o atributo `title` do input na mensagem de erro
@@ -29,11 +30,14 @@ if (login) {
             }
         }
 
+        // Mostra a mensage caso houver erro
         if (erro) {
             alert(mensagem);
+            e.preventDefault();
             return false;
         }
 
+        // Submete o formulário caso tudo OK
         return true;
     }
 }
@@ -45,7 +49,7 @@ if (login) {
  * @return {boolean}
  */
 function podeValidar(input) {
-    var tipos = ['text', 'password', 'textarea', 'email', 'number'];
+    var tipos = ['text', 'password', 'textarea', 'email', 'number', 'file'];
 
     // Loop em todos os tipos de input que podem ser validados
     for (var i = 0; i < tipos.length; i++) {
